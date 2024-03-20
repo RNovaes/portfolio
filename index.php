a06e526c8d97701c8d598735c15c447afc3707ab
RNovaes <natinnovaes@hotmail.com>
2024-03-20 18:13:21 -0300

    Novo trabalho

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Renato Novaes</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="index.css" />
+    </head>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div id="MeusDados">
+
+            <img src="imagens/Renato.jpg" class="photo">
+            <hr class="linha">
+
+            <div class="contatos">
+
+                <h1 class="Fonte">Renato Novaes</h1>
+                <div class="redes">
+                    <img src="imagens/instagram.webp" class="logo">
+                    <p>@novaesrenato</p>
+                </div>
+                <div class="redes">
+                    <img src="imagens/facelogo.png" class="logo">
+                    <p>Renato Novaes</p>
+                </div>
+                <div class="redes">
+                    <img src="imagens/whats.webp" class="logo">
+                    <p>(21) 96687-6893</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
