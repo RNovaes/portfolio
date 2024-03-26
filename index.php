dc60e165ffb73bed89c1d1123f91a711f7171727
RNovaes <natinnovaes@hotmail.com>
2024-03-25 21:18:53 -0300

    Atualizando

--- index.php
+++ index.php
@@ -24,7 +24,20 @@
 
             <div class="contatos">
 
-                <h1 class="Fonte">Renato Novaes</h1>
+                <h1 class="fonte">Renato Novaes</h1>
+                <p class="textoPessoal">Atuo na área de tecnologia como programador freelancer.
+                    Minha jornada nesse campo teve início em 2020,<br>
+                    quando iniciei minha graduação e mergulhei no mundo da programação.
+                    Desde então,<br> tenho adquirido habilidades em algumas linguagens,
+                    como C++, HTML, CSS, JavaScript e PHP,<br> trabalhando tanto no
+                    desenvolvimento Frontend quanto Backend. Hoje, além de dominar<br>
+                    essas linguagens, estou envolvido em vários projetos
+                    que me desafiam e me permitem crescer constantemente.
+                </p>
+            </div>
+
+            <div class="novocontato">
+
                 <div class="redes">
                     <img src="imagens/instagram.webp" class="logo">
                     <p>@novaesrenato</p>
@@ -34,12 +47,94 @@
                     <p>Renato Novaes</p>
                 </div>
                 <div class="redes">
-                    <img src="imagens/whats.webp" class="logo">
+                    <img src="imagens/whatsapp.png" class="logo">
                     <p>(21) 96687-6893</p>
+                </div>
+                <div class="redes">
+                    <img src="imagens/email.png" class="logo">
+                    <p>novaesrenatodev@gmail.com</p>
+                </div>
+                <div class="redes">
+                    <img src="imagens/link.png" class="logo">
+                    <p>Renato Marcus Bispo Novaes</p>
                 </div>
 
             </div>
 
+        </div>
+
+        <div id="box">
+            <div id="habilidades">
+
+                <div class="habil">
+                    <h1 class="fonte">Habilidades</h1>
+
+                    <div class="butoes">
+                        <div class="butt">
+                            <span>HTML</span>
+                        </div>
+                        <div class="butt">
+                            <span>CSS</span>
+                        </div>
+                        <div class="butt">
+                            <span>PHP</span>
+                        </div>
+                        <div class="butt">
+                            <span>SQL</span>
+                        </div>
+                        <div class="butt">
+                            <span>C</span>
+                        </div>
+                    </div>
+
+                    <div class="butoes">
+                        <div class="butt">
+                            <span>JavaScript</span>
+                        </div>
+                        <div class="butt">
+                            <span>React</span>
+                        </div>
+                        <div class="butt">
+                            <span>React Native</span>
+                        </div>
+                        <div class="butt">
+                            <span>NodeJs</span>
+                        </div>
+                        <div class="butt">
+                            <span>Python</span>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div id="academico">
+
+                <div class="dadosAcademicos">
+                    <h1 class="fonte">Acadêmico</h1>
+
+                    <div class="formacao">
+                        <label>Curso de Inglês</label>
+                        <label>Cultura Inglesa</label>
+                        <label>Intermediário</label>
+                        <label>Concluído</label>
+                    </div>
+                    <div class="formacao">
+                        <label>Ciência da Computação</label>
+                        <label>Unisuam</label>
+                        <label>Bacharel</label>
+                        <label>Concluído</label>
+                    </div>
+                    <div class="formacao">
+                        <label>Curso MySQL</label>
+                        <label>Curso Guanabara</label>
+                        <label>40 Horas</label>
+                        <label>Concluído</label>
+                    </div>
+                </div>
+
+            </div>
         </div>
 
     </div>
